4472e384eb1a2bb79415cb0a1d5d181463023d06
AlooJeanbon <125971045+AlooJeanbon@users.noreply.github.com>
2024-01-23 22:53:46 +0100

    le cuir sans e

--- Client/src/Accueil.jsx
+++ Client/src/Accueil.jsx
@@ -14,7 +14,7 @@ function Accueil(){
                     <img src={img} alt='' className='CitationImage'/>
                 </div>
                 <div className='text-container-type-a'>
-                    <p className='text'>Depuis la nuit des temps les gens ont communiqué. Au début, ce n'était que des gestes, puis des bruits et seulement depuis quelques millénaires, les langues. Les langages ont permis, à travers l'histoire, de galvaniser les foules. Grâce à la langue les gens ont évolué pour devenir meilleurs à travers de grands discours. Et dans ces discours, des phrases, plus marquantes que d'autres qui nous sont parvenu jusqu'à aujourd'hui. Afin de ne pas oublier les précédentes et les suivantes nous avons créer ce site qui a pour but de contenir toutes les phrases les plus mémorables de l'humanité.</p>
+                    <p className='text'>Depuis la nuit des temps les êtres humains communiquent. Au début, ce n'était que des gestes, puis des bruits et seulement depuis quelques millénaires, les langues. Les langages ont permis, à travers l'histoire, de galvaniser les foules. Grâce à la langue les humains ont évolué pour devenir meilleurs à travers de grands discours. Et dans ces discours, des phrases, plus marquantes que d'autres, qui nous sont parvenues jusqu'à aujourd'hui. Afin de ne pas oublier les précédentes et les suivantes nous avons créé ce site ayant pour but de contenir toutes les phrases les plus mémorables de l'humanité.</p>
                 </div>
             </div>
             <BottomPage />
